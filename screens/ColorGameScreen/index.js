cbfb68e245c38095e4224509c646d39988e99966
narmincebiyeva <narmincebiyeva@gmail.com>
2020-07-02 14:11:04 +0400

    color game

--- screens/ColorGameScreen/index.js
+++ screens/ColorGameScreen/index.js
@@ -1,57 +1,112 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState,useEffect,useRef} from 'react';
+import { StyleSheet,  View, TouchableOpacity,Image } from 'react-native';
 
 import { Colors,  } from "../../utils/colorList"
 import { createNumber  } from "../../utils/createNumber"
-
 import { CustomText,CustomBtn } from '../../components'
+import {ColorCard} from "./ColorCard"
+import {EndModal} from "./EndModal"
 
-
-export const ColorGameScreen = ( ) => {
-
-    const randomInd = () => {
+const randomInd = () => {
         const length = Colors.length;
         return createNumber(1,length)
     }
   
+export const ColorGameScreen = ( ) => {
+
+   const [modal, setModal] = useState(false)
    const [color, setColor] = useState( Colors[randomInd()]);
    const [name, setName] = useState( Colors[randomInd()]);
-   const [points, setPoints] = useState(0)
+   const [boxColor, setBoxColor] = useState([color,name]);
+   const [points, setPoints] = useState(0);
+   const [wrongs, setWrongs] = useState(0);
 
+   const box = boxColor[createNumber(1,2)];
+   const openedTimer = useRef(null);
+
+   
    const clickHandler = (value) => {
-    setColor(Colors[randomInd()]);
-    setName(Colors[randomInd()]);
-    if(value === name) {
-        setPoints(points+1);
+    const randomColor = Colors[randomInd()];
+    const randomName = Colors[randomInd()]
+    setColor(randomColor);
+    setName(randomName);
+    setBoxColor([randomColor,randomName]);
+    
+    if(value === "yes"){
+         if(box === name) {
+             setPoints(points+1);
+         }
+         else {
+            setWrongs(wrongs+1);
+         }
+     }
+     if(value === "no"){
+        if(box === color) {
+            setPoints(points+1);
+        }
+        else {
+            setWrongs(wrongs+1);
+        }
+    }
+    if(wrongs === 4) {
+        setModal(true)
     }
 
+   };
 
-   }
+   const resetGame = () => {
+    setModal(false);
+    setPoints(0);
+    gameTimer();
+   }; 
 
 
-  return( 
+   const gameTimer = () => {
+    if (openedTimer.current) {
+        clearTimeout(openedTimer.current);
+      }
+      openedTimer.current = setTimeout(() => setModal(true), 20000);
+     }
+
+   useEffect(() => {
+    gameTimer();
+  }, []);
+
+
+   return( 
+
     <View style={styles.container}>
-         <CustomText weight="bold">
-                {points}
+            <CustomText weight="bold">
+                {points} / {wrongs}
             </CustomText> 
-        <View style={styles.colorContainer}>
-            <CustomText weight="bold" style={[styles.colorName,{color:color}]}>
-                {name}
-            </CustomText>    
-        </View>
 
-        <View style={styles.btnContainer}>
+        <ColorCard
+         color= {color}
+         name={name}
+         box={box}
+         />
+
+        <View style={styles.btnContainer}> 
+                             
             <CustomBtn
+             color="red"
              style={styles.btn}
-             title={color}
-             onPress={()=> clickHandler(color)}
+             title="No"
+             onPress={()=> {clickHandler("no");}}
              />
             <CustomBtn
+            color="green"
              style={styles.btn} 
-             title={name} 
-             onPress={()=> clickHandler(name)}/>
+             title="yes" 
+             onPress={()=> {clickHandler("yes")}}/>
         </View>
-    </View>
+        <EndModal  
+          visible={modal}
+          close={() => resetGame()}
+          points={points}
+        />
+       </View>
+   
   )
 };
 
@@ -59,20 +114,13 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        backgroundColor: "#edf9fa"
     },
-     colorContainer: {
-         width: 200,
-         height: 200,
-         justifyContent:"center",
-         alignItems: "center", 
-     },
+
     btn: {
        width: 100,
        margin: 10
-    },
-    colorName: {
-        fontSize: 40,
     },
     btnContainer: {
         flexDirection: "row"
