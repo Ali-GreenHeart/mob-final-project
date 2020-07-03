a917e0b3c6e5cbc23eb08fe2d920b42243a91642
narmincebiyeva <narmincebiyeva@gmail.com>
2020-07-03 21:58:54 +0400

    trafficlight game

--- screens/index.js
+++ screens/index.js
@@ -2,4 +2,6 @@ export {GameScreen} from "./GameScreen.js"
 export {PasswordGameScreen} from "./PasswordGameScreen"
 export {ColorGameScreen} from "./ColorGameScreen"
 export { MemoryGridGameScreen } from "./MemoryGridGameScreen"
+export { TrafficLightGameScreen } from "./TrafficLightGameScreen"
+
 
