eec589e1420f58bd0541bf7673c08be5d17cfe84
OrxanQurbanzade <qurbanli00.qo@gmail.com>
2020-07-08 11:05:55 +0400

    added random function to Alpharef game

--- screens/AlphaRefGame/BoardView.js
+++ screens/AlphaRefGame/BoardView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,  } from 'react-native';
 import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
@@ -11,6 +11,27 @@ let CELL_PADDING = Math.floor(CELL_SIZE * .05); // 5% of the cell size
 let BORDER_RADIUS = CELL_PADDING * 2;
 let TILE_SIZE = CELL_SIZE - CELL_PADDING * 2;
 let LETTER_SIZE = Math.floor(TILE_SIZE * .75);
+
+function shuffle(array) {
+    let i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    return array;
+}
+
+let ranNums = shuffle([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
 
 export class BoardView extends React.Component{
     render() {
@@ -24,12 +45,12 @@ export class BoardView extends React.Component{
         for (let row = 0; row < SIZE; row++) {
             for (let col = 0; col < SIZE; col++) {
                 let key = row * SIZE + col;
-                let letter = String.fromCharCode(65 + key);
+                let letter = String.fromCharCode(65 + ranNums[key]);
                 let position = {
                     left: col * CELL_SIZE + CELL_PADDING,
                     top: row * CELL_SIZE + CELL_PADDING
                 };
-                result.push(this.renderTile(key, position, letter))
+                result.push(this.renderTile(ranNums[key], position, letter))
             }
         }
         return result;
