a4b891a1dbc7acc62945bfe4b2f60fa7d9a08812
gunelaliyeva <38286683+gunelaliyeva@users.noreply.github.com>
2020-07-10 14:10:28 +0400

    Update index.js

--- screens/Auth/index.js
+++ screens/Auth/index.js
@@ -8,7 +8,7 @@ import {
     TouchableWithoutFeedback,
     Dimensions
 } from 'react-native';
-\import {CustomBtn, CustomHeader, CustomText} from "../../components";
+import {CustomBtn, CustomHeader, CustomText} from "../../components";
 import fbApp from "../../store/firebase";
 import {signIn} from "../../store/userCredentials";
 import store from "../../store";
